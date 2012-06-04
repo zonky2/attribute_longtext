4f5214073f9fca9f6abf29d8f3b28d14526d4ee7
David Maack <maack@men-at-work.de>
2012-06-04 14:27:29 +0200

    Added attribut longtext and translatedlongtext

---
+++ src/system/modules/metamodelsattribute_longtext/MetaModelAttributeLongtext.php
@@ -0,0 +1,68 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ * @package     MetaModels
+ * @subpackage  AttributeText
+ * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright   CyberSpectrum
+ * @license     private
+ * @filesource
+ */
+
+/**
+ * This is the MetaModelAttribute class for handling text fields.
+ * 
+ * @package     MetaModels
+ * @subpackage  AttributeText
+ * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
+ */
+class MetaModelAttributeLongtext extends MetaModelAttributeSimple
+{
+
+    public function getSQLDataType()
+    {
+        return 'text NULL';
+    }
+
+    public static function getAttributeSettingNames()
+    {
+        return array_merge(parent::getAttributeSettingNames(), array(
+                    'parentCheckbox',
+                    'titleField',
+                    'width50',
+                    'insertBreak',
+                    'sortingField',
+                    'filteredField',
+                    'searchableField',
+                    'mandatory',
+                    'defValue',
+                    'uniqueItem',
+                    'formatPrePost',
+                    'format',
+                    'editGroups'
+                ));
+    }
+
+    public function getFieldDefinition()
+    {
+        $arrFieldDef = parent::getFieldDefinition();
+        $arrFieldDef['inputType'] = 'longtext';
+        return $arrFieldDef;
+    }
+
+    public function parseValue($arrRowData, $strOutputFormat = 'html')
+    {
+        $arrResult = parent::parseValue($arrRowData, $strOutputFormat);
+        $arrResult['html'] = $arrRowData[$this->getColName()];
+        return $arrResult;
+    }
+
+}
+
+?>
---
+++ src/system/modules/metamodelsattribute_longtext/dca/tl_metamodel_attribute.php
@@ -0,0 +1,10 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Table tl_metamodel_attribute 
+ */
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['metapalettes']['longtext extends _simpleattribute_'] = array
+        (
+//	'+display' => array('titleField after name')
+);
+?>
