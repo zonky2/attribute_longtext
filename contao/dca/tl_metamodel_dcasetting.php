efa087fe0528db6396f2dd92f143414cbdd91951
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-01-28 21:28:49 +0100

    Drop obsolete fields.

--- contao/dca/tl_metamodel_dcasetting.php
+++ contao/dca/tl_metamodel_dcasetting.php
@@ -34,7 +34,5 @@ $GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id'
     'overview'     => array(
         'filterable',
         'searchable',
-        'sortable',
-        'flag'
     )
 );
