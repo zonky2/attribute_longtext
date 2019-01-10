d40f1b8e932fd94036691b745896fea7ca7cc774
Christian Schiffler <c.schiffler@cyberspectrum.de>
2019-01-10 13:52:50 +0100

    Move tests to main dir

--- tests/DependencyInjection/MetaModelsAttributeLongtextExtensionTest.php
+++ tests/DependencyInjection/MetaModelsAttributeLongtextExtensionTest.php
@@ -13,6 +13,7 @@
  * @package    MetaModels
  * @subpackage AttributeLongtext
  * @author     David Molineus <david.molineus@netzmacht.de>
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @copyright  2012-2017 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_text/blob/master/LICENSE LGPL-3.0
  * @filesource
